31d0a9e15e1a842d2381dedd863d1f359c8c7a20
Shian-Yu <157192752+Shian-Yu@users.noreply.github.com>
2024-09-13 16:58:00 +0800

    upload

---
+++ app/about/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+        <div>
+            <button className="border border-black px-3 py-2">X</button>
+            <button className="border border-black px-3 py-2">X</button>
+            <button className="border border-black px-3 py-2">X</button>
+        </div>
+        <div>
+            <button className="border border-black px-3 py-2">X</button>
+            <button className="border border-black px-3 py-2">X</button>
+            <button className="border border-black px-3 py-2">X</button>
+        </div>
+        <div>
+            <button className="border border-black px-3 py-2">X</button>
+            <button className="border border-black px-3 py-2">X</button>
+            <button className="border border-black px-3 py-2">X</button>
+        </div>
+    </main>
+  );
+}
