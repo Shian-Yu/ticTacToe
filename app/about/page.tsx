2960b91db831e2a2b6ee3c888769dff3fbdce8a8
shian <dannyxo1@yahoo.com.tw>
2024-10-24 18:05:54 +0800

    井字遊戲第一版

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,73 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from 'react';
+
+function Square({value, onSquareClick}: {value: string, onSquareClick: () => void}) {
+  return <button className="border border-black w-10 h-10 flex items-center justify-center" onClick={onSquareClick}>{ value }</button>
+}
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i: number) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  function calculateWinner(squares: Array<string | null>) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   return (
-    <main className="">
-        <div>
-            <button className="border border-black px-3 py-2">X</button>
-            <button className="border border-black px-3 py-2">X</button>
-            <button className="border border-black px-3 py-2">X</button>
-        </div>
-        <div>
-            <button className="border border-black px-3 py-2">X</button>
-            <button className="border border-black px-3 py-2">X</button>
-            <button className="border border-black px-3 py-2">X</button>
-        </div>
-        <div>
-            <button className="border border-black px-3 py-2">X</button>
-            <button className="border border-black px-3 py-2">X</button>
-            <button className="border border-black px-3 py-2">X</button>
-        </div>
+    <main className="w-[120px] h-[120px] flex flex-col space-y-0">
+      <div>{status}</div>
+      <div className="flex space-x-0">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      <div className="flex space-x-0">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+      <div className="flex space-x-0">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
     </main>
   );
 }
